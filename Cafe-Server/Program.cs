ad7b6dfe716b49267c7db27ea8c8735a4ecfe71b
nikit <nikit@DESKTOP-0U71F0B>
2023-02-22 15:25:00 +0300

    Added CRUD for menu item

--- Cafe-Server/Program.cs
+++ Cafe-Server/Program.cs
@@ -13,7 +13,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World");
 
-
 app.MapGet("/api/categories", (MasterContext db) => db.Categories.ToList());
 
 app.MapPost("/api/categories", async (Category category, MasterContext db) =>
@@ -52,6 +51,53 @@ app.MapPut("/api/categories", async (Category categoryData, MasterContext db) =>
     await db.SaveChangesAsync();
     return Results.Json(category);
 
-}); 
+});
+
+app.MapGet("/api/menuitems", (MasterContext db) => db.MenuItems.ToList());
+
+app.MapPost("/api/menuitems", async (MenuItem menuItem, MasterContext db) =>
+{
+    // добавл€ем пользовател€ в массив
+    await db.MenuItems.AddAsync(menuItem);
+    await db.SaveChangesAsync();
+    return menuItem;
+});
+
+app.MapDelete("/api/menuitems/{id:int}", async (int id, MasterContext db) =>
+{
+    MenuItem? menuItem = await db.MenuItems.FirstOrDefaultAsync(m => m.Id == id);
+
+    // если не найден, отправл€ем статусный код и сообщение об ошибке
+    if (menuItem == null) return Results.NotFound(new { message = "ѕозици€ меню не найдена" });
+
+    // если найден, удал€ем его
+    db.MenuItems.Remove(menuItem);
+
+    await db.SaveChangesAsync();
+
+    return Results.Json(menuItem);
+});
+
+app.MapPut("/api/menuitems", async (MenuItem menuItemData, MasterContext db) => {
+
+    // получаем  по id
+    var menuItem = await db.MenuItems.FirstOrDefaultAsync(u => u.Id == menuItemData.Id);
+
+    // если не найден, отправл€ем статусный код и сообщение об ошибке
+    if (menuItem == null) return Results.NotFound(new { message = " атегори€ не найдена" });
+
+    // если  найден, измен€ем его данные и отправл€ем обратно клиенту
+    menuItem.Name = menuItemData.Name;
+    menuItem.Weight = menuItemData.Weight;
+    menuItem.Ingredients = menuItemData.Ingredients;
+    menuItem.Caloric = menuItemData.Caloric;
+    menuItem.Price = menuItemData.Price;
+    menuItem.Available = menuItemData.Available;
+    menuItem.CategoryId = menuItemData.CategoryId;
+
+    await db.SaveChangesAsync();
+    return Results.Json(menuItem);
+
+});
 
 app.Run();
